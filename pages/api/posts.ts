2dad95e4d36512c39978ae32e9b424a449a09373
Shaun Church <shaun@shaunchurch.com>
2020-10-07 09:50:18 +0100

    Demo Prisma and React query

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,5 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
-};
+const prisma = new PrismaClient();
+
+export default async function getPosts(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // await prisma.post.create({
+  //   data: {
+  //     title: "My Third Post Title",
+  //     body: "This is my third amazing post!",
+  //   },
+  // });
+  const posts = await prisma.post.findMany();
+  res.status(200).json(posts);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
-
 import Link from "next/link";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+import { useQuery } from "react-query";
+import { Post } from "@prisma/client";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   return {
@@ -10,13 +12,26 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
+async function getPosts() {
+  const res = await fetch("http://localhost:3000/api/posts");
+  return await res.json();
+}
+
 export default function IndexRoute({ content }) {
+  const { isLoading, error, data } = useQuery<Post[]>("post", getPosts);
+  const { data: data2 } = useQuery<Post[]>("post-2", getPosts);
+
   return (
     <main>
-      Hello world! {content}
+      <ul>
+        {data?.map((post) => {
+          return <li>{post.title}</li>;
+        })}
+      </ul>
       <Link href="/pagetwo">
         <a>Page Two</a>
       </Link>
+      <ReactQueryDevtools initialIsOpen />
     </main>
   );
 }
